ef3aeb2978b3433f885ecca570259d47a6396528
janath <janathhma@gmail.com>
2023-12-17 07:42:07 +0530

    add js functions to the home page

---
+++ asserts/js/app.js
@@ -0,0 +1,388 @@
+/* import { products } from "./item-list.js"; */
+const products = [
+  {
+    code: "B1001",
+    name: "Classic Burger (Large)",
+    image: "1.PNG",
+    price: 750.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1002",
+    name: "Classic Burger (Regular",
+    image: "2.PNG",
+    price: 1500.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1003",
+    name: " Turkey Burger",
+    image: "3.PNG",
+    price: 1600.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1004",
+    name: "Chicken Burger (Large)",
+    image: "4.PNG",
+    price: 1400.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1005",
+    name: "Chicken Burger (Regular)",
+    image: "5.PNG",
+    price: 800.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1006",
+    name: "Cheese Burger(Large)",
+    image: "6.PNG",
+    price: 1000.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1007",
+    name: "Cheese Burger(Regular)",
+    image: "3.PNG",
+    price: 600.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1008",
+    name: "Bacon Burger",
+    image: "4.PNG",
+    price: 650.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1009",
+    name: "Shawarma Burger",
+    image: "5.PNG",
+    price: 800.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1010",
+    name: "Olive Burger",
+    image: "6.PNG",
+    price: 18000,
+    category: "Burgers",
+  },
+  {
+    code: "B1012",
+    name: "Double-Cheese Burger",
+    image: "7.PNG",
+    price: 1250.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1013",
+    name: "Crispy Chicken Burger(Regular)",
+    image: "8.PNG",
+    price: 1200.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1014",
+    name: "Crispy Chicken Burger(Large)",
+    image: "9.PNG",
+    price: 1600.0,
+    category: "Burgers",
+  },
+  {
+    code: "B1015",
+    name: "Paneer Burger",
+    image: "10.PNG",
+    price: 900,
+    category: "Burgers",
+  },
+  {
+    code: "B1016",
+    name: " CrispyChickenSubmarine(Large)",
+    image: "11.PNG",
+    price: 2000,
+    category: "Submarines",
+  },
+  {
+    code: "B1017",
+    name: " CrispyChickenSubmarine(Regular)",
+    image: "12.PNG",
+    price: 1500.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1018",
+    name: " ChickenSubmarine(Large)",
+    image: "13.PNG",
+    price: 1800.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1019",
+    name: "ChickenSubmarine(Regular)",
+    image: "14.PNG",
+    price: 1400.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1020",
+    name: "GrinderSubmarine",
+    image: "15.PNG",
+    price: 2300.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1021",
+    name: " CheeseSubmarine",
+    image: "16.PNG",
+    price: 2200.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1022",
+    name: "DoubleCheesenChickenSubmarine",
+    image: "17.PNG",
+    price: 1900.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1023",
+    name: "SpecialHorgieSubmarine",
+    image: "18.PNG",
+    price: 2800.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1024",
+    name: " MOSSpecialSubmarine",
+    image: "19.PNG",
+    price: 3000.0,
+    category: "Submarines",
+  },
+  {
+    code: "B1025",
+    name: "SteakFries(Large)",
+    image: "20.PNG",
+    price: 1200.0,
+    category: "Fries",
+  },
+  {
+    code: "B1026",
+    name: "SteakFries(Medium)",
+    image: "21.PNG",
+    price: 600.0,
+    category: "Fries",
+  },
+  {
+    code: "B1027",
+    name: "FrenchFries(Large)",
+    image: "22.PNG",
+    price: 800.0,
+    category: "Fries",
+  },
+  {
+    code: "B1028",
+    name: " FrenchFries(Medium)",
+    image: "23.PNG",
+    price: 650.0,
+    category: "Fries",
+  },
+  {
+    code: "B1029",
+    name: "FrenchFries(Small)",
+    image: "24.PNG",
+    price: 450.0,
+    category: "Fries",
+  },
+  {
+    code: "B1030",
+    name: " SweetPotatoFries(Large)",
+    image: "25.PNG",
+    price: 600.0,
+    category: "Fries",
+  },
+  {
+    code: "B1031",
+    name: "ChickennCheesePasta",
+    image: "26.PNG",
+    price: 1600.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1032",
+    name: "ChickenPennePasta",
+    image: "27.PNG",
+    price: 1700.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1033",
+    name: "GroundTurkeyPastaBake",
+    image: "28.PNG",
+    price: 2900.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1034",
+    name: "CreamyShrimpPasta",
+    image: "29.PNG",
+    price: 2000.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1035",
+    name: " LemonButterPasta",
+    image: "30.PNG",
+    price: 1950.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1036",
+    name: "TagliatellePasta",
+    image: "31.PNG",
+    price: 2400.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1037",
+    name: "BakedRavioli",
+    image: "32.PNG",
+    price: 2000.0,
+    category: "Pasta",
+  },
+  {
+    code: "B1038",
+    name: " FriedChicken(Small)",
+    image: "33.PNG",
+    price: 1200.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1039",
+    name: "FriedChicken(Regular)",
+    image: "34.PNG",
+    price: 2300.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1040",
+    name: "FriedChicken(Large)",
+    image: "35.PNG",
+    price: 3100.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1041",
+    name: "HotWings(Large)",
+    image: "36.PNG",
+    price: 2400.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1042",
+    name: " DevilledChicken(Large)",
+    image: "37.PNG",
+    price: 900.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1043",
+    name: "BBQChicken(Regular)",
+    image: "38.PNG",
+    price: 2100.0,
+    category: "Chicken",
+  },
+  {
+    code: "B1044",
+    name: "Pepsi(330ml)",
+    image: "39.PNG",
+    price: 990.0,
+    category: "Beverages",
+  },
+  {
+    code: "B1045",
+    name: "Coca-Cola(330ml)",
+    image: "40.PNG",
+    price: 1230.0,
+    category: "Beverages",
+  },
+  {
+    code: "B1046",
+    name: "Sprite(330ml)",
+    image: "41.PNG",
+    price: 1500.0,
+    category: "Beverages",
+  },
+  {
+    code: "B1047",
+    name: "Mirinda(330ml)",
+    image: "42.PNG",
+    price: 850.0,
+    category: "Beverages",
+  },
+];
+const productsContainer = document.querySelector(".order-section");
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItem(products);
+  displayMenuButtons();
+});
+
+function displayMenuItem(menuitems) {
+  let displayMenu = menuitems.map((value, index) => {
+    return `
+  <div class="item col-lg-3  col-sm-4 col-xs-5">
+  <img src="image/${value.image}">
+  <div class="title">${value.name}</div>
+  <div class="price">${value.price.toLocaleString()}</div>
+  <button onclick="addToCard(${index})">Add To Card</button>
+  </div>
+  `;
+  });
+  displayMenu = displayMenu.join("");
+  productsContainer.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  const categoriesContainer = document.querySelector(".categories");
+  const categories = products.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["All"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+            ${category}
+          </button>`;
+    })
+    .join("");
+
+  categoriesContainer.innerHTML = categoryBtns;
+  const filterBtns = categoriesContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = products.filter(function (menuitems) {
+        // console.log(menuItem.category);
+        if (menuitems.category === category) {
+          return menuitems;
+        }
+      });
+      if (category === "All") {
+        displayMenuItem(products);
+      } else {
+        displayMenuItem(menuCategory);
+      }
+    });
+  });
+}
