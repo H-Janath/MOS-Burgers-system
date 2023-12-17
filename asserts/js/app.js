d31ad1aff01e9c4ae27cda4913dfb7d8091a0b65
janath <janathhma@gmail.com>
2023-12-17 12:33:09 +0530

    Add cart

--- asserts/js/app.js
+++ asserts/js/app.js
@@ -386,3 +386,20 @@ function displayMenuButtons() {
     });
   });
 }
+let iconCart = document.querySelector(".iconCart");
+let cart = document.querySelector(".cart");
+let container = document.querySelector(".home");
+let close = document.querySelector(".close");
+iconCart.addEventListener("click", function () {
+  if (cart.style.right == "-100%") {
+    cart.style.right = "0";
+    /* container.style.transform = "translateX(-400px)"; */
+  } else {
+    cart.style.right = "-100%";
+    /* container.style.transform = "translateX(0)"; */
+  }
+});
+close.addEventListener("click", function () {
+  cart.style.right = "-100%";
+  container.style.transform = "translateX(0)";
+});
